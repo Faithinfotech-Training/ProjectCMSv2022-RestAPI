7c4e5f474070696d3debe6c6493552b2c1caae36
sahadan <sahadan@gmail.com>
2022-05-06 13:29:59 +0530

    second Commit

--- src/main/java/com/faith/app/DemoProejctCmsAndRest1Application.java
+++ src/main/java/com/faith/app/DemoProejctCmsAndRest1Application.java
@@ -8,6 +8,7 @@ public class DemoProejctCmsAndRest1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProejctCmsAndRest1Application.class, args);
+		System.out.println("After generated Personal Access WenToken");
 	}
 
 }
